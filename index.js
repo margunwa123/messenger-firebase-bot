5c1357c3a7a9d812f6c410ab084237004427348e
margunwa123 <13518114@std.stei.itb.ac.id>
2021-04-14 12:04:03 +0700

    added response message

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { handleMessage, handlePostback } = require("./message-handler");
 
 const PORT = process.env.PORT || 1337;
 const app = express();
@@ -47,6 +48,14 @@ app.post("/webhook", (req, res) => {
 
       let sender_psid = webhook_event.sender.id;
       console.log("SENDER PSID: " + sender_psid);
+
+      // check if the event is a message or postback and
+      //pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
     });
 
     // Returns a '200 OK' response to all requests
--- message-handler.js
+++ message-handler.js
@@ -1,8 +1,49 @@
+const request = require("request");
+
 // Handles messages events
-export function handleMessage(sender_psid, received_message) {}
+export function handleMessage(sender_psid, received_message) {
+  let response;
+
+  // check if the message contains text
+  if (received_message.text) {
+    // create the payload for a basic text message
+    response = {
+      text: `You sent the message ${received_message.text}. Now send me an image!`,
+    };
+  }
+
+  callSendAPI(sender_psid, response);
+}
 
 // Handles messaging_postbacks events
 export function handlePostback(sender_psid, received_postback) {}
 
 // Sends response messages via the Send API
-export function callSendAPI(sender_psid, response) {}
+export function callSendAPI(sender_psid, response) {
+  // construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
+
+  // send http request to the messenger platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: {
+        access_token: process.env.PAGE_ACCESS_TOKEN,
+      },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message " + err);
+      }
+    }
+  );
+}
